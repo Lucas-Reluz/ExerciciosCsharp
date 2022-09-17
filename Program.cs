cf3e1c26262351484e274d41ecd701c74f27f863
Lucas Reluz <lucas.reluz@localize.com.br>
2022-09-17 16:18:12 -0300

    @Lucas-Reluz [Feito exercicio simples de media de idade] {16:17 | 17/09/2022}

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿var quantidadeAlunos = 0;
+var pausa = true;
+while (pausa == true)
+{
+    Console.WriteLine("Informe a idade dos 5 alunos: ");
+    quantidadeAlunos++;
+    var idade = Convert.ToInt32(Console.ReadLine());
+    if (quantidadeAlunos == 5)
+    {
+        var media = (idade + idade + idade + idade + idade)/5;
+        Console.WriteLine($"A media de idade dos alunos é {media}");
+        pausa = false;
+    }
+}
+//Exercício basico para calcular a media da idade de 5 alunos, tentei fazer da menor forma possível.
